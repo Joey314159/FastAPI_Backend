c8d7e3aa87a0ca988aee1898fabd94573466ac5c
JoelsGit <joelduran305@yahoo.com>
2026-04-20 23:44:15 -0700

    I added a List of Books that holds a dictionary with information about the books

--- CRUD_FastAPI_Books/Books.py
+++ CRUD_FastAPI_Books/Books.py
@@ -3,6 +3,15 @@ from fastapi import FastAPI
 # Allows uvicorn understand that we are creating a new app object
 app = FastAPI()
 
+BOOKS = [
+    {"Title: ": "Title One", "Author: ": "Author One", "Category: ": "Science"},
+    {"Title: ": "Title Two", "Author: ": "Author Two", "Category: ": "Science"},
+    {"Title: ": "Title Three", "Author: ": "Author Three", "Category: ": "Science"},
+    {"Title: ": "Title Four", "Author: ": "Author Four", "Category: ": "Science"},
+    {"Title: ": "Title Five", "Author: ": "Author Five", "Category: ": "Science"},
+    {"Title: ": "Title Six", "Author: ": "Author Six", "Category: ": "Science"},
+]
+
 
 @app.get("/")
 async def firstFN():
