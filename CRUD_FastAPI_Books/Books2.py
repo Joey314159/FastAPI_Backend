5d4c1f91fed2e5837de1bc0bc951d18a20c374d6
JoelsGit <joelduran305@yahoo.com>
2026-04-28 23:45:28 -0700

    Added data validation to some of the fields in our book

--- CRUD_FastAPI_Books/Books2.py
+++ CRUD_FastAPI_Books/Books2.py
@@ -22,9 +22,9 @@ class Book:
 class BookRequest(BaseModel):
     id: int
     title: str = Field(min_length=3)
-    author: str
-    description: str
-    rating: int
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=120)
+    rating: int = Field(gt=-1, lt=6)
 
 
 BOOKS = [
