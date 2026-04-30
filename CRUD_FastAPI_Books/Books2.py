cf0ba9d258abb1443623db649c58962bbdddc91f
JoelsGit <joelduran305@yahoo.com>
2026-04-29 23:18:55 -0700

    Creating a model_config that will help with information being displayed in Swagger UI

--- CRUD_FastAPI_Books/Books2.py
+++ CRUD_FastAPI_Books/Books2.py
@@ -21,12 +21,23 @@ class Book:
 
 
 class BookRequest(BaseModel):
-    id: Optional[int] = None
+    id: Optional[int] = Field(description="ID is not needed on create", default=None)
     title: str = Field(min_length=3)
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=120)
     # greater than 1, less than 6
     rating: int = Field(gt=-1, lt=6)
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "New Book",
+                "author": "codingWithRoby",
+                "description": "A very good book",
+                "rating": "",
+            }
+        }
+    }
 
 
 BOOKS = [
