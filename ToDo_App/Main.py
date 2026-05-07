620c04e41a01999710fc05f617762b91e01e1239
JoelsGit <joelduran305@yahoo.com>
2026-05-06 20:34:35 -0700

    Added a way to call the db and return all info on it

--- ToDo_App/Main.py
+++ ToDo_App/Main.py
@@ -1,7 +1,23 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
 import Models
-from Database import engine
+from Models import Todos
+from Database import SessionLocal, engine
 
 app = FastAPI()
 
 Models.Base.metadata.create_all(bind=engine)
+
+
+def getDB():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/")
+async def readAll(db: Annotated[Session, Depends(getDB)]):
+    return db.query(Todos).all()
