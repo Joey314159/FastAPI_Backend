872ed7e5b1ec8931aaa0e347f332dfa2bb495d24
JoelsGit <joelduran305@yahoo.com>
2026-05-07 16:05:54 -0700

    I added a new API endpoint which is to retriev items based on their IDs using an API dependancy as well as raising HTTP Exceptions and finally adding a way to validate user input

--- ToDo_App/Main.py
+++ ToDo_App/Main.py
@@ -1,7 +1,8 @@
 from typing import Annotated
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException, Path
 from sqlalchemy.orm import Session
 import Models
+from starlette import status
 from Models import Todos
 from Database import SessionLocal, engine
 
@@ -13,11 +14,29 @@ Models.Base.metadata.create_all(bind=engine)
 def getDB():
     db = SessionLocal()
     try:
+        # Pauses the function and gives the session to your route
         yield db
     finally:
+        # Ensures we never leave a Database connection open by closing it immediately afterwards
         db.close()
 
 
-@app.get("/")
-async def readAll(db: Annotated[Session, Depends(getDB)]):
+# First dependancy injection
+dbDependancy = Annotated[Session, Depends(getDB)]
+#                                 ^^^^^^^^^^^^^---- Tells FastAPI 'whenever a route needs this, automatically run getDB() and inject the results'
+#    This is helpful because instead of each route managing its own database session; we define it once and FastAPI
+#    knows to write it in wherever you ask for it
+
+
+@app.get("/", status_code=status.HTTP_200_OK)
+async def readAll(db: dbDependancy):
     return db.query(Todos).all()
+
+
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def readTodo(db: dbDependancy, todo_id: int = Path(gt=0)):
+    todoModel = db.query(Todos).filter(Todos.id == todo_id).first()
+
+    if todoModel is not None:
+        return todoModel
+    raise HTTPException(status_code=404, detail="Todo ID not found")
