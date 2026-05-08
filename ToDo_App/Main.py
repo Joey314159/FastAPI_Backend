f7574fb42a19e63714ed0fbd9e036915ff5128e8
JoelsGit <joelduran305@yahoo.com>
2026-05-07 19:04:29 -0700

    I added a new put api endpoint that allows me to modify the contents of an already existing to do object

--- ToDo_App/Main.py
+++ ToDo_App/Main.py
@@ -1,5 +1,6 @@
 from typing import Annotated
 from fastapi import Depends, FastAPI, HTTPException, Path
+from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 import Models
 from starlette import status
@@ -28,6 +29,13 @@ dbDependancy = Annotated[Session, Depends(getDB)]
 #    knows to write it in wherever you ask for it
 
 
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3, max_length=30)
+    description: str = Field(min_length=3, max_length=100)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool
+
+
 @app.get("/", status_code=status.HTTP_200_OK)
 async def readAll(db: dbDependancy):
     return db.query(Todos).all()
@@ -40,3 +48,28 @@ async def readTodo(db: dbDependancy, todo_id: int = Path(gt=0)):
     if todoModel is not None:
         return todoModel
     raise HTTPException(status_code=404, detail="Todo ID not found")
+
+
+@app.post("/todo/", status_code=status.HTTP_201_CREATED)
+async def createTodo(db: dbDependancy, todoRequest: TodoRequest):
+    todoModel = Todos(**todoRequest.model_dump())
+    db.add(todoModel)
+    db.commit()
+
+
+@app.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def updateTodo(
+    db: dbDependancy, todoRequest: TodoRequest, todo_id: int = Path(gt=0)
+):
+    todoModel = db.query(Todos).filter(Todos.id == todo_id).first()
+
+    if todoModel is None:
+        raise HTTPException(status_code=404, detail="To Do ID not found")
+
+    todoModel.title = todoRequest.title
+    todoModel.description = todoRequest.description
+    todoModel.priority = todoRequest.priority
+    todoModel.complete = todoRequest.complete
+
+    db.add(todoModel)
+    db.commit()
