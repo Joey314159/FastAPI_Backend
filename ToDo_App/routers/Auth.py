b414504dc1fc5c5de419d867431bfe333fbd29d1
JoelsGit <joelduran305@yahoo.com>
2026-05-11 17:30:37 -0700

    Added some minor changes

--- ToDo_App/routers/Auth.py
+++ ToDo_App/routers/Auth.py
@@ -1,8 +1,29 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from Models import Users
 
 router = APIRouter()
 
 
-@router.get("/auth/")
-async def getUser():
-    return {"user": "authenticated"}
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    firstName: str
+    lastName: str
+    password: str
+    role: str
+
+
+@router.post("/auth")
+async def createUser(createUserRequest: CreateUserRequest):
+    createUserModel = Users(
+        email=createUserRequest.email,
+        username=createUserRequest.username,
+        firstName=createUserRequest.firstName,
+        lastName=createUserRequest.lastName,
+        role=createUserRequest.role,
+        hashedPWD=createUserRequest.password,
+        isActive=True,
+    )
+
+    return createUserModel
